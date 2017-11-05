c519cd8fb6e9611a167d576cb763f533ccc1669c
sndstudy <sanada2054ss@gmail.com>
2017-11-05 20:05:36 +0900

    first commit

---
+++ KeyakiBlogScraper.py
@@ -0,0 +1,63 @@
+"""
+欅坂の公式ブログから画像を取得するモジュール
+"""
+import urllib.request
+import urllib.error
+import os
+import datetime
+import KeyakiMember
+from bs4 import BeautifulSoup
+
+def create_directory(directory_name):
+    """
+    create_directory関数
+    ディレクトリが既に存在している場合は何もしない
+    """
+    if os.path.isdir("./" + directory_name):
+        return True
+    else:
+        os.mkdir("./" + directory_name)
+        return True
+
+def get_today_datetime():
+    """
+    get_today_datetime関数
+    今日の日時をYYYYMMDDhhmmで取得する
+    """
+    today = datetime.datetime.today()
+
+    return  str(today.year)+str(today.month)+str(today.day)+str(today.hour)+str(today.minute)
+
+
+def main():
+    """
+    main関数
+    """
+    member_num_str = "13"
+
+    # アクセスするURL(欅坂の公式ブログのURL)
+    url = "http://www.keyakizaka46.com/s/k46o/diary/member/list?ima=0000&ct=" + member_num_str
+
+    # URLにアクセスする htmlが帰ってくる
+    html = urllib.request.urlopen(url)
+
+    # htmlをBeautifulSoupで扱う
+    soup = BeautifulSoup(html, "html.parser")
+
+    member_name = KeyakiMember.get_member_name(member_num_str)
+
+    create_directory(member_name)
+
+    # 個人ブログ最新記事内のimgタグを取得する
+    img_tags = soup.find("div", class_="box-article").find_all("img")
+
+    img_cnt = 0
+    today_str = get_today_datetime()
+
+    for img_tag in img_tags:
+        img_src = img_tag.get("src")
+        urllib.request.urlretrieve(img_src, "./" + member_name +  "/" +
+                                   today_str + member_name + str(img_cnt) + ".jpg")
+        img_cnt += 1
+
+main()
---
+++ KeyakiMember.py
@@ -0,0 +1,50 @@
+"""
+欅坂のメンバーを定義するモジュール
+"""
+MEMBER_LIST = {"01":"ishimori_nijika",
+               "02":"imaizumi_yui",
+               "03":"uemura_rina",
+               "04":"ozeki_rika",
+               "05":"oda_nana",
+               "06":"koike_minami",
+               "07":"kobayashi_yui",
+               "08":"saitou_fuyuka",
+               "09":"satou_shiori",
+               "10":"shida_manaka",
+               "11":"sugai_yuuka",
+               "12":"suzumoto_miyu",
+               "13":"nagasawa_nanako",
+               "14":"habu_miduho",
+               "15":"harada_aoi",
+               "17":"hirate_yurina",
+               "18":"moriya_akane",
+               "19":"yonetani_nanami",
+               "20":"watanabe_rika",
+               "21":"watanabe_risa",
+               "22":"nagahama_neru",
+               "23":"iguchi_mao",
+               "24":"ushio_sarina",
+               "25":"kakizaki_memi",
+               "26":"kageyama_yuuka",
+               "27":"katou_shiho",
+               "28":"saitou_kyouko",
+               "29":"sasaki_kumi",
+               "30":"sasaki_mirei",
+               "31":"takase_mana",
+               "32":"takamoto_ayaka",
+               "33":"higashimura_mei",
+               "34":"kanemura_miku",
+               "35":"kawata_hina",
+               "36":"kosaka_nao",
+               "37":"tomita_suzuka",
+               "38":"nibu_akari",
+               "39":"hamagishi_hiyori",
+               "40":"matsuda_konoka",
+               "41":"miyata_manamo",
+               "42":"watanabe_miho"}
+
+def get_member_name(member_num):
+    """
+    欅坂のメンバーを定義するモジュール
+    """
+    return MEMBER_LIST[member_num]
