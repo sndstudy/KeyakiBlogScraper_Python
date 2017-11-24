d5a6ca19d734eb6e772f857dc9e7e6cd19a40469
sndstudy <sanada2054ss@gmail.com>
2017-11-24 22:39:17 +0900

    コメント追加

--- TestKeyakiMember.py
+++ TestKeyakiMember.py
@@ -5,8 +5,15 @@ import unittest
 from KeyakiMember import get_member_name
 
 class TestKeyakiMember(unittest.TestCase):
-
+    """
+    KeyakiMember.py
+    テストクラス
+    """
     def test_get_member_name(self):
+        """
+        指定した値をもとにメンバーの名前を取得することを
+        確認するテストケース
+        """
         member_number = "33"
         return_str = "higashimura_mei"
         member_name = get_member_name(member_number)
